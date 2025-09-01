e22528358163af329a96a28104d46aae2ced1c80
Zubair Uddin <zubair.uddin@jamipartners.com>
2025-09-01 19:44:09 +0500

    Added x unit

---
+++ WPFCICDDemo.XTests/WPFCICDDemo.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace WPFCICDDemo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(10, 20, 30)]
+        public void Add_MultipleCases_ReturnsSum(int a, int b, int expected)
+        {
+            Assert.Equal(expected, a + b);
+        }
+    }
+}
