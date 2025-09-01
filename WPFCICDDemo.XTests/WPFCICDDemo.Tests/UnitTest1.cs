266198a5f1ade10847b3feb8d2de103fc3dd9bcf
Zubair Uddin <zubair.uddin@jamipartners.com>
2025-09-01 20:37:08 +0500

    Added testcase

--- WPFCICDDemo.XTests/WPFCICDDemo.Tests/UnitTest1.cs
+++ WPFCICDDemo.XTests/WPFCICDDemo.Tests/UnitTest1.cs
@@ -19,5 +19,14 @@ namespace WPFCICDDemo.Tests
 
         }
 
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(10, 20, 30)]
+        public void Add_MultipleCases_ReturnsSum2(int a, int b, int expected)
+        {
+            Assert.Equal(expected, a + b);
+
+        }
+
     }
 }
